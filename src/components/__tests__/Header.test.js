502d227c19351a7b78624951d23d3f05c76df583
lav <kumarlav59@gmail.com>
2024-03-07 12:00:03 +0530

    Header Test cases

---
+++ src/components/__tests__/Header.test.js
@@ -0,0 +1,55 @@
+import { render ,screen} from "@testing-library/react"
+import Header from "../Header"
+import { Provider } from "react-redux";
+import appStore from "../../utils/appStore";
+import { BrowserRouter } from "react-router-dom";
+import "@testing-library/jest-dom";
+
+test("Should render Header component with login button",()=>{
+    render(
+        <BrowserRouter>
+        <Provider store={appStore}>
+            <Header/>
+        </Provider>
+        </BrowserRouter>
+        
+    );
+    
+    const loginButton = screen.getByRole("button",{name:"Login"});
+    //const loginButton = screen.getByText("Login");
+    //Assertiom
+    expect(loginButton).toBeInTheDocument();
+
+});
+
+test("Should render Header component with Cart items 0",()=>{
+    render(
+        <BrowserRouter>
+        <Provider store={appStore}>
+            <Header/>
+        </Provider>
+        </BrowserRouter>
+        
+    );
+    
+    const cartItems = screen.getByText("Cart - (0 items)");
+    //Assertiom
+    expect(cartItems).toBeInTheDocument();
+
+});
+
+test("Should render Header component with Cart item",()=>{
+    render(
+        <BrowserRouter>
+        <Provider store={appStore}>
+            <Header/>
+        </Provider>
+        </BrowserRouter>
+        
+    );
+    
+    const cartItems = screen.getByText(/Cart/);
+    //Assertiom
+    expect(cartItems).toBeInTheDocument();
+
+})
